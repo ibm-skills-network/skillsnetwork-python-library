ada2a5e402b401267f3c53f28a2e611f5de8d608
Sam Prokopchuk <Sam.Prokopchuk@ibm.com>
2022-07-21 13:20:34 -0400

    Update version in conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Bradley Steinfeld, Sam Prokopchuk, James Reeve"
 author = "Bradley Steinfeld, Sam Prokopchuk, James Reeve"
 
 # The full version, including alpha/beta/rc tags
-release = "0.20.3"
+release = "0.20.4"
 
 
 # -- General configuration ---------------------------------------------------
