6359c61ea495de860171a0863c98c0de3949d8e7
Rui Zhu <rui.zhu@ibm.com>
2023-07-24 10:31:54 -0400

    use ubuntu 18.04 for test pre-commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="skillsnetwork",
+    version="0.20.7-rc1",
+    author="Rui Zhu",
+    author_email="Rui.Zhu@ibm.com",
+    description="SkillsNetwork Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
